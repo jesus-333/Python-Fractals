a05f1e0b1ec27ef5cadb882df674f8a1e321f445
Jesus <albi25.zanca@gmail.com>
2023-03-24 13:17:14 +0100

    Minor update

---
+++ fractals_V2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import numba
+
+"""
+%load_ext autoreload
+%autoreload 2
+
+"""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def compute_Mandelbrot():
+    pass
